732a82d7063c729339aa3ce5ada206ea3d5c527a
Invisibilities <78047131+iInvisibilities@users.noreply.github.com>
2024-02-09 18:18:05 +0100

    added semi-functional home page

--- index.js
+++ index.js
@@ -4,18 +4,26 @@ const iframeWindow = document.getElementsByTagName("iframe")[0];
 for (let i = 0; i < navElements.length; i++) {
     const element = navElements[i];
     element.onclick = (e) => {
+        if(!element.hasAttribute("href")) return;
         newPage = element.attributes.getNamedItem("href").textContent;
         iframeWindow.src = newPage;
-        cleanSelection();
+        clearClass("current");
+        clearClass("inside");
         element.classList.add("current");
+
+        let parent = element.parentElement;
+        if(parent.nodeName == "UL") {
+            parent.parentElement.classList.add("inside");
+        }
+
         return false;
     }
 }
 
-function cleanSelection() {
-    let array = document.getElementsByClassName("current");
+function clearClass(cName) {
+    let array = document.getElementsByClassName(cName);
     for (let i = 0; i < array.length; i++) {
         const element = array[i];
-        element.classList.remove("current");
+        element.classList.remove(cName);
     }
 }
